ce50dc2f5df134a089f25a99e11a2f4c19b93685
learn-web-and-earn <asadullah54347@gmail.com>
2025-08-23 09:25:41 +0500

    Initial Setup Complete

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+export default app;
